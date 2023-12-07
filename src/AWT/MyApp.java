32d62fc5db67220b52d4dc2d8d6b5aee1e04268f
SingHuyLeang <leang6525@gmail.com>
2023-12-07 19:35:24 +0700

    Finish

--- src/AWT/MyApp.java
+++ src/AWT/MyApp.java
@@ -3,15 +3,12 @@ package AWT;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowEvent;
-import java.awt.event.WindowListener;
+import java.awt.event.*;
 
-public class MyApp extends Frame implements WindowListener, ActionListener {
+public class MyApp extends Frame implements WindowListener, ActionListener, MouseListener {
     // build app ( constructor )
     TextField txtId,txtName,txtQty,txtPrice,txtTotal;
-    Button btnCash,btnAdd,btnUpdate,btnDelete,btnPayment;
+    Button btnCash,btnAdd,btnUpdate,btnDelete,btnClear;
     JTable table;
     DefaultTableModel model;
     public MyApp(){
@@ -83,6 +80,7 @@ public class MyApp extends Frame implements WindowListener, ActionListener {
         JScrollPane js = new JScrollPane(table);
         js.setBounds(600,100,500,280);
         add(js);
+        table.addMouseListener(this);
 
         // Update
         btnUpdate= new Button("Update");
@@ -99,12 +97,12 @@ public class MyApp extends Frame implements WindowListener, ActionListener {
         add(btnDelete);
         btnDelete.addActionListener(this);
         // Payment
-        btnPayment= new Button("Payment");
-        btnPayment.setBounds(940,400,155,40);
-        btnPayment.setFont(new Font("",Font.PLAIN,24));
-        btnPayment.setBackground(new Color(43, 72, 246));
-        add(btnPayment);
-        btnPayment.addActionListener(this);
+        btnClear= new Button("Clear");
+        btnClear.setBounds(940,400,155,40);
+        btnClear.setFont(new Font("",Font.PLAIN,24));
+        btnClear.setBackground(new Color(43, 72, 246));
+        add(btnClear);
+        btnClear.addActionListener(this);
 
         // UI
         setLayout(null);
@@ -132,6 +130,14 @@ public class MyApp extends Frame implements WindowListener, ActionListener {
             };
             model.addRow(row);
         });
+    }
+    // Clear Text field
+    public void clear(){
+        txtId.setText("");
+        txtName.setText("");
+        txtQty.setText("");
+        txtPrice.setText("");
+        txtTotal.setText("");
     }
 
     @Override
@@ -184,6 +190,57 @@ public class MyApp extends Frame implements WindowListener, ActionListener {
             double total = qty * price;
             Shop.list.add(new Shop(id,name,qty,price,total));
             getData();
+            clear();
+        } else if (e.getSource() == btnUpdate) {
+            int id = Integer.parseInt(txtId.getText());
+            String name = txtName.getText();
+            int qty = Integer.parseInt(txtQty.getText());
+            double price = Double.parseDouble(txtPrice.getText());
+            double total = qty * price;
+            int index = table.getSelectedRow();
+            Shop.list.set(index,new Shop(id,name,qty,price,total));
+            getData();
+            clear();
+        } else if (e.getSource() == btnDelete) {
+            int index = table.getSelectedRow();
+            Shop.list.remove(index);
+            getData();
+            clear();
+        } else if (e.getSource() == btnClear) {
+            Shop.list.clear();
+            getData();
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        DefaultTableModel model = (DefaultTableModel) table.getModel();
+        int row = table.getSelectedRow();
+
+        txtId.setText(model.getValueAt(row,0).toString());
+        txtName.setText(model.getValueAt(row,1).toString());
+        txtQty.setText(model.getValueAt(row,2).toString());
+        txtPrice.setText(model.getValueAt(row,3).toString());
+        txtTotal.setText(model.getValueAt(row,4).toString());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
